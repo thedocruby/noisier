75758a358fb4e13b241ac1fae75de07e3cba239d
Dr. Rubisco <76263371+thedocruby@users.noreply.github.com>
2022-03-02 17:34:36 -0500

    More abstractions; start Simplex implementation

--- src/noisier.rs
+++ src/noisier.rs
@@ -1,46 +1,78 @@
 use rand::prelude::*;
 use std::{f64::consts::PI, fmt};
 
-pub trait NoiseMachine {
+pub trait NoiseMachine<'a> {
     fn new() -> Self;
     fn from_seed(seed: u64) -> Self;
-    fn set_seed(&mut self, seed: u64);
-    fn seed(&self) -> u64;
-    fn sample_raw(&self, dim: usize, pos: &Vec<f64>) -> f64;
+    fn set_seed(&'a mut self, seed: u64);
+    fn seed(&'a self) -> u64;
+    fn sample_raw(&'a self, dim: usize, pos: &Vec<f64>) -> f64;
     fn sample(
-        &self,
+        &'a self,
         dim: usize,
         pos: &Vec<f64>,
         scale: &Vec<f64>,
         offset: &Vec<f64>,
         weight: f64,
         bias: f64,
-    ) -> f64;
+    ) -> f64 {
+        let dim = dim.clamp(1, usize::BITS as usize);
+        assert_eq!(pos.len(), dim);
+        assert_eq!(scale.len(), dim);
+        assert_eq!(offset.len(), dim);
+        self.sample_raw(dim, &{
+            let mut new_pos = Vec::with_capacity(dim);
+            for i in 0..(dim) {
+                new_pos.push(pos[i].clamp(0.0, 1.0) * scale[i].abs() + offset[i].abs());
+            }
+            new_pos
+        }) * weight
+            + bias
+    }
     fn sample_tileable(
-        &self,
+        &'a self,
         dim: usize,
         pos: &Vec<f64>,
         scale: &Vec<f64>,
         offset: &Vec<f64>,
         weight: f64,
         bias: f64,
-    ) -> f64;
-    fn generate_map(
-        &self,
+    ) -> f64 {
+        let dim2 = (dim * 2).clamp(1, usize::BITS as usize);
+        let dim = dim2 / 2;
+        assert_eq!(pos.len(), dim);
+        assert_eq!(scale.len(), dim);
+        assert_eq!(offset.len(), dim);
+        self.sample_raw(dim2, &{
+            let mut new_pos = Vec::with_capacity(dim2);
+            for i in 0..(dim) {
+                let pi_pos = pos[i].clamp(0.0, 1.0) * 2.0 * PI;
+                new_pos.push((pi_pos.cos() + 1.0) / (2.0 * PI) * scale[i] + offset[i]);
+            }
+            for i in 0..(dim) {
+                let pi_pos = pos[i].clamp(0.0, 1.0) * 2.0 * PI;
+                new_pos.push((pi_pos.sin() + 1.0) / (2.0 * PI) * scale[i].abs() + offset[i].abs());
+            }
+            new_pos
+        }) * weight
+            + bias
+    }
+    fn generate_buffer<T>(
+        &'a self,
         dim: usize,
         scale: &Vec<f64>,
         offset: &Vec<f64>,
         weight: f64,
         bias: f64,
-    ) -> NoiseMap<Self> {
-        NoiseMap::<Self> {
-            machine: self,
+    ) -> T where T: NoiseBuffer<'a, Self> {
+        T::new(
+            self,
             dim,
-            scale: scale.clone(),
-            offset: offset.clone(),
+            scale.clone(),
+            offset.clone(),
             weight,
             bias,
-        }
+        )
     }
 }
 
@@ -89,7 +121,7 @@ impl PerlinMachine {
         vector
     }
 }
-impl NoiseMachine for PerlinMachine {
+impl <'a> NoiseMachine<'a> for PerlinMachine {
     fn new() -> Self {
         let seed = thread_rng().gen();
         let mut rng = StdRng::seed_from_u64(seed);
@@ -103,21 +135,22 @@ impl NoiseMachine for PerlinMachine {
         rng.fill(&mut randoms);
         Self { seed, randoms }
     }
-    fn set_seed(&mut self, seed: u64) {
+    fn set_seed(&'a mut self, seed: u64) {
         let mut rng = StdRng::seed_from_u64(seed);
         let mut randoms = [0; usize::BITS as usize];
         rng.fill(&mut randoms);
         self.seed = seed;
         self.randoms = randoms;
     }
-    fn seed(&self) -> u64 {
+    fn seed(&'a self) -> u64 {
         self.seed
     }
-    fn sample_raw(&self, dim: usize, pos: &Vec<f64>) -> f64 {
+    fn sample_raw(&'a self, dim: usize, pos: &Vec<f64>) -> f64 {
+        assert_eq!(pos.len(), dim);
         let dim = dim.clamp(1, usize::BITS as usize);
         let pos: Vec<f64> = pos
             .iter()
-            .map(|x| x.abs().clamp(0.0, u32::MAX as f64))
+            .map(|x| x.abs().clamp(0.0, (u32::MAX - usize::BITS) as f64))
             .collect();
         let mut pos0 = Vec::with_capacity(dim);
         let mut δ_pos = Vec::with_capacity(dim);
@@ -145,58 +178,87 @@ impl NoiseMachine for PerlinMachine {
         }
         out[0]
     }
-    fn sample(
-        &self,
-        dim: usize,
-        pos: &Vec<f64>,
-        scale: &Vec<f64>,
-        offset: &Vec<f64>,
-        weight: f64,
-        bias: f64,
-    ) -> f64 {
-        let dim = dim.clamp(1, usize::BITS as usize);
-        self.sample_raw(dim, &{
-            let mut new_pos = Vec::with_capacity(dim);
-            for i in 0..(dim) {
-                new_pos.push(pos[i].clamp(0.0, 1.0) * scale[i].abs() + offset[i].abs());
-            }
-            new_pos
-        }) * weight
-            + bias
-    }
-    fn sample_tileable(
-        &self,
-        dim: usize,
-        pos: &Vec<f64>,
-        scale: &Vec<f64>,
-        offset: &Vec<f64>,
-        weight: f64,
-        bias: f64,
-    ) -> f64 {
-        let dim2 = dim * 2;
-        self.sample_raw(dim2, &{
-            let mut new_pos = Vec::with_capacity(dim2);
-            for i in 0..(dim) {
-                let pi_pos = pos[i].clamp(0.0, 1.0) * 2.0 * PI;
-                new_pos.push((pi_pos.cos() + 1.0) / (2.0 * PI) * scale[i] + offset[i]);
-            }
-            for i in 0..(dim) {
-                let pi_pos = pos[i].clamp(0.0, 1.0) * 2.0 * PI;
-                new_pos.push((pi_pos.sin() + 1.0) / (2.0 * PI) * scale[i].abs() + offset[i].abs());
-            }
-            new_pos
-        }) * weight
-            + bias
-    }
 }
 impl fmt::Debug for PerlinMachine {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "NoiseMachine{{seed: {}}}", self.seed)
+        write!(f, "PerlinMachine{{seed: {}}}", self.seed)
     }
 }
 
+#[derive(Clone)]
+pub struct SimplexMachine where {
+    seed: u64,
+    randoms: [usize; usize::BITS as usize],
+}
+impl <'a> NoiseMachine<'a> for SimplexMachine{
+    fn new() -> Self {
+        let seed = thread_rng().gen();
+        let mut rng = StdRng::seed_from_u64(seed);
+        let mut randoms = [0; usize::BITS as usize];
+        rng.fill(&mut randoms);
+        Self { seed, randoms }
+    }
+    fn from_seed(seed: u64) -> Self {
+        let mut rng = StdRng::seed_from_u64(seed);
+        let mut randoms = [0; usize::BITS as usize];
+        rng.fill(&mut randoms);
+        Self { seed, randoms }
+    }
+    fn set_seed(&'a mut self, seed: u64) {
+        let mut rng = StdRng::seed_from_u64(seed);
+        let mut randoms = [0; usize::BITS as usize];
+        rng.fill(&mut randoms);
+        self.seed = seed;
+        self.randoms = randoms;
+    }
+    fn seed(&'a self) -> u64 {
+        self.seed
+    }
+    fn sample_raw(&'a self, dim: usize, pos: &Vec<f64>) -> f64 {
+        let dim = dim.clamp(1, usize::BITS as usize);
+        assert_eq!(pos.len(), dim);
+        let mut pos: Vec<f64> = pos
+            .iter()
+            .map(|x| x.abs().clamp(0.0, (u32::MAX - usize::BITS) as f64))
+            .collect();
+        let f = ((dim as f64 + 1.0).sqrt() - 1.0) / dim as f64;
+        let g = (1.0 - 1.0/(dim as f64 + 1.0).sqrt()) / dim as f64;
+        let sum: f64 = pos.iter().sum();
+        let mut pos0 = Vec::with_capacity(dim);
+        let mut δ_pos = Vec::with_capacity(dim);
+        for i in 0..dim{
+            pos[i] += sum * f;
+            pos0.push(pos[i].floor() as u32);
+            δ_pos.push(pos[i] - pos0[i] as f64);
+        }
+        todo!()
+    }
+}
+impl fmt::Debug for SimplexMachine {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "SimplexMachine{{seed: {}}}", self.seed)
+    }
+}
+
+pub trait NoiseBuffer<'a, T> where T: NoiseMachine<'a> + ?Sized {
+    fn sample(&'a self, pos: &Vec<f64>) -> f64;
+    fn machine(&'a self) -> &'a T;
+    fn dim(&'a self) -> usize;
+    fn scale(&'a self) -> Vec<f64>;
+    fn offset(&'a self) -> Vec<f64>;
+    fn weight(&'a self) -> f64;
+    fn bias(&'a self) -> f64;
+    fn new(
+        machine: &'a T,
+        dim: usize,
+        scale: Vec<f64>,
+        offset: Vec<f64>,
+        weight: f64,
+        bias: f64,
+    ) -> Self;
+}
 #[derive(Clone, Debug)]
-pub struct NoiseMap<'a, T> where T: NoiseMachine + ?Sized {
+pub struct ContinuousNoise<'a, T> where T: NoiseMachine<'a> + ?Sized {
     machine: &'a T,
     dim: usize,
     scale: Vec<f64>,
@@ -205,8 +267,8 @@ pub struct NoiseMap<'a, T> where T: NoiseMachine + ?Sized {
     bias: f64,
 }
 
-impl<'a, T> NoiseMap<'a, T>  where T: NoiseMachine {
-    pub fn sample(&self, pos: &Vec<f64>) -> f64 {
+impl<'a, T> NoiseBuffer<'a, T> for ContinuousNoise<'a, T> where T: NoiseMachine<'a> + ?Sized {
+    fn sample(&'a self, pos: &Vec<f64>) -> f64 {
         self.machine.sample(
             self.dim,
             pos,
@@ -216,32 +278,102 @@ impl<'a, T> NoiseMap<'a, T>  where T: NoiseMachine {
             self.bias,
         )
     }
-    pub fn sample_tileable(&self, pos: &Vec<f64>) -> f64 {
-        self.machine.sample(
-            self.dim,
-            pos,
-            &self.scale,
-            &self.offset,
-            self.weight,
-            self.bias,
-        )
-    }
-    pub fn machine(&self) -> &'a T {
+    fn machine(&'a self) -> &'a T {
         self.machine
     }
-    pub fn dim(&self) -> usize {
+    fn dim(&'a self) -> usize {
         self.dim.clone()
     }
-    pub fn scale(&self) -> Vec<f64> {
+    fn scale(&'a self) -> Vec<f64> {
         self.scale.clone()
     }
-    pub fn offset(&self) -> Vec<f64> {
+    fn offset(&'a self) -> Vec<f64> {
         self.offset.clone()
     }
-    pub fn weight(&self) -> f64 {
+    fn weight(&'a self) -> f64 {
         self.weight.clone()
     }
-    pub fn bias(&self) -> f64 {
+    fn bias(&'a self) -> f64 {
         self.bias.clone()
+    }
+    fn new(
+        machine: &'a T,
+        dim: usize,
+        scale: Vec<f64>,
+        offset: Vec<f64>,
+        weight: f64,
+        bias: f64,
+    ) -> Self {
+        let dim = dim.clamp(1, usize::BITS as usize);
+        assert_eq!(scale.len(), dim);
+        assert_eq!(offset.len(), dim);
+        Self {
+            machine,
+            dim,
+            scale,
+            offset,
+            weight,
+            bias,
+        }
+    }
+}
+#[derive(Clone, Debug)]
+pub struct RecurrentNoise<'a, T> where T: NoiseMachine<'a> + ?Sized {
+    machine: &'a T,
+    dim: usize,
+    scale: Vec<f64>,
+    offset: Vec<f64>,
+    weight: f64,
+    bias: f64,
+}
+
+impl<'a, T> NoiseBuffer<'a, T> for RecurrentNoise<'a, T> where T: NoiseMachine<'a> + ?Sized {
+    fn sample(&self, pos: &Vec<f64>) -> f64 {
+        self.machine.sample_tileable(
+            self.dim,
+            pos,
+            &self.scale,
+            &self.offset,
+            self.weight,
+            self.bias,
+        )
+    }
+    fn machine(&self) -> &'a T {
+        self.machine
+    }
+    fn dim(&self) -> usize {
+        self.dim.clone()
+    }
+    fn scale(&self) -> Vec<f64> {
+        self.scale.clone()
+    }
+    fn offset(&self) -> Vec<f64> {
+        self.offset.clone()
+    }
+    fn weight(&self) -> f64 {
+        self.weight.clone()
+    }
+    fn bias(&self) -> f64 {
+        self.bias.clone()
+    }
+    fn new(
+        machine: &'a T,
+        dim: usize,
+        scale: Vec<f64>,
+        offset: Vec<f64>,
+        weight: f64,
+        bias: f64,
+    ) -> Self {
+        let dim = (dim * 2).clamp(1, usize::BITS as usize) / 2;
+        assert_eq!(scale.len(), dim);
+        assert_eq!(offset.len(), dim);
+        Self {
+            machine,
+            dim,
+            scale,
+            offset,
+            weight,
+            bias,
+        }
     }
 }

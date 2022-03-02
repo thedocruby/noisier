f4d896de1408eb08b84a4cd854987cc453e4ffe3
Dr. Rubisco <76263371+thedocruby@users.noreply.github.com>
2022-03-01 21:24:42 -0500

    Initial commit, huge progress
    Almost done, gonna turn in for the night

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,5 @@
-//! A simple perlin noise implementation in Rust
-//!
-//! # Example
-//! ```rust
-//! extern crate perlin_noise as perlin;
-//! use perlin::PerlinNoise;
-//!
-//! fn main() {
-//!   let perlin = PerlinNoise::new();
-//!   println!("{}",perlin.get(132.2));
-//!   println!("{}",perlin.get2d([12.0,32.0]));
-//!   println!("{}",perlin.get3d([12.0,32.0,25.0]));
-//! }
-//! ```
-//!  
-//!
-//!
-
 extern crate rand;
 
-mod perlin;
+mod perlin_plus;
 
-pub use perlin::PerlinNoise;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        use super::PerlinNoise;
-        let perlin = PerlinNoise::new();
-
-        perlin.get(12.2);
-        perlin.get2d([12.0, 32.0]);
-        perlin.get3d([12.0, 32.0, 25.0]);
-
-        assert_eq!(perlin.get(0.0), 0.46875);
-    }
-}
+pub use perlin_plus::NoiseMachine;
